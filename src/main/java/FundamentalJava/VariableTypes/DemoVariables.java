209df69e18d096e80efdb8bec9ac63d5c5f3de31
Manojkumar886 <maddymano434@gmail.com>
2023-04-27 11:02:48 +0530

    Instance and LOcal variable is done

--- src/main/java/FundamentalJava/VariableTypes/DemoVariables.java
+++ src/main/java/FundamentalJava/VariableTypes/DemoVariables.java
@@ -1,2 +1,23 @@
-package FundamentalJava.VariableTypes;public class DemoVariables {
+package FundamentalJava.VariableTypes;
+
+public class DemoVariables
+{
+    public static void main(String[] args)
+    {
+//        Object creation
+//        Classname ObjectName=new Classname();
+
+
+        int alpha=97;//Instance  Variable
+        char beta=(char) alpha;//Narrowing
+        System.out.println("my number is :" +alpha+" \t 97 number key value is :"+beta);
+        DemoVariables Obj=new DemoVariables();//object creation
+        Obj.LocalPerforamnce();
+    }
+    public void LocalPerforamnce()//user defined method
+    {
+        String username="Manojkumar434";//Local variable
+        String password="Maddy@123";
+        System.out.println(username+password);
+    }
 }
